55fcb784078b45b38e5908b1ec66c96b1aec7da9
christianmkuss <33375981+christianmkuss@users.noreply.github.com>
2018-08-07 10:58:43 -0700

    Added three more servos

--- motorController.py
+++ motorController.py
@@ -15,26 +15,38 @@ arduinoSerialData = serial.Serial('/dev/ttyUSB0', 9600)
 GPIO.setmode(GPIO.BOARD)
 
 # Which GPIO pin to which motor
-waterMotor = 11
-Motor1Pin = 13
-Motor2Pin = 16
-Motor3Pin = 18
+waterMotor = 29
+Servo1Pin = 31
+Servo2Pin = 32
+Servo3Pin = 33
+Servo4Pin = 35
+Servo5Pin = 36
+Servo6Pin = 37
 
 # Specify them as outputs
-GPIO.setup(11, GPIO.OUT)
-GPIO.setup(13, GPIO.OUT)
-GPIO.setup(16, GPIO.OUT)
-GPIO.setup(18, GPIO.OUT)
-Motor1 = GPIO.PWM(Motor1Pin, 50)
-Motor2 = GPIO.PWM(Motor2Pin, 50)
-Motor3 = GPIO.PWM(Motor3Pin, 50)
-water = GPIO.PWM(waterMotor, 50)
+GPIO.setup(waterMotor, GPIO.OUT)
+GPIO.setup(Servo1Pin, GPIO.OUT)
+GPIO.setup(Servo2Pin, GPIO.OUT)
+GPIO.setup(Servo3Pin, GPIO.OUT)
+GPIO.setup(Servo4Pin, GPIO.OUT)
+GPIO.setup(Servo5Pin, GPIO.OUT)
+GPIO.setup(Servo6Pin, GPIO.OUT)
+
+Servo1 = GPIO.PWM(Servo1Pin, 50)
+Servo2 = GPIO.PWM(Servo2Pin, 50)
+Servo3 = GPIO.PWM(Servo3Pin, 50)
+Servo4 = GPIO.PWM(Servo1Pin, 50)
+Servo5 = GPIO.PWM(Servo2Pin, 50)
+Servo6 = GPIO.PWM(Servo3Pin, 50)
 
 # Turn everything off
-Motor1.start(0)
-Motor2.start(0)
-Motor3.start(0)
-water.start(0)
+Servo1.start(0)
+Servo2.start(0)
+Servo3.start(0)
+Servo4.start(0)
+Servo5.start(0)
+Servo6.start(0)
+GPIO.output(waterMotor, GPIO.LOW)
 
 # We in this for the long haul
 while True:
@@ -42,12 +54,26 @@ while True:
     pill1_hour_list = []
     pill1_minute_list = []
     pill1_numpills = []
+
     pill2_hour_list = []
     pill2_minute_list = []
     pill2_numpills = []
+
     pill3_hour_list = []
     pill3_minute_list = []
     pill3_numpills = []
+
+    pill4_hour_list = []
+    pill4_minute_list = []
+    pill4_numpills = []
+
+    pill5_hour_list = []
+    pill5_minute_list = []
+    pill5_numpills = []
+
+    pill6_hour_list = []
+    pill6_minute_list = []
+    pill6_numpills = []
 
     # Get the website data from the URL
     r = requests.get("http://0.0.0.0:80")
@@ -60,9 +86,16 @@ while True:
     pill1 = page_soup.findAll("td", {"class": "pill1Time"})
     pill2 = page_soup.findAll("td", {"class": "pill2Time"})
     pill3 = page_soup.findAll("td", {"class": "pill3Time"})
+    pill4 = page_soup.findAll("td", {"class": "pill4Time"})
+    pill5 = page_soup.findAll("td", {"class": "pill5Time"})
+    pill6 = page_soup.findAll("td", {"class": "pill6Time"})
+
     numpill1 = page_soup.findAll("td", {"class": "pill1num"})
     numpill2 = page_soup.findAll("td", {"class": "pill2num"})
     numpill3 = page_soup.findAll("td", {"class": "pill3num"})
+    numpill4 = page_soup.findAll("td", {"class": "pill4num"})
+    numpill5 = page_soup.findAll("td", {"class": "pill5num"})
+    numpill6 = page_soup.findAll("td", {"class": "pill6num"})
 
     # Loop through all elements in the list of pill times and store them in lists
     for x in range(len(pill1)):
@@ -92,47 +125,111 @@ while True:
         pill3_hour_list.append(this_time[0])
         pill3_minute_list.append(this_time[2])
 
+    for x in range(len(pill4)):
+        # Add the number of pills in the list
+        pill4_numpills.append(int(numpill4[x].text))
+
+        # Split the pill times into hours and minutes and add to respective lists
+        this_time = pill4[x].text.split()
+        pill4_hour_list.append(this_time[0])
+        pill4_minute_list.append(this_time[2])
+
+    for x in range(len(pill5)):
+        # Add the number of pills in the list
+        pill5_numpills.append(int(numpill5[x].text))
+
+        # Split the pill times into hours and minutes and add to respective lists
+        this_time = pill5[x].text.split()
+        pill5_hour_list.append(this_time[0])
+        pill5_minute_list.append(this_time[2])
+
+    for x in range(len(pill6)):
+        # Add the number of pills in the list
+        pill6_numpills.append(int(numpill6[x].text))
+
+        # Split the pill times into hours and minutes and add to respective lists
+        this_time = pill6[x].text.split()
+        pill6_hour_list.append(this_time[0])
+        pill6_minute_list.append(this_time[2])
+
     # Update the current time
     now = datetime.datetime.now()
     print(now)
     sleep = 0
     watering = False
+
     for x in range(len(pill1_hour_list)):
         # Compare the current hour to the hour in the list and the current minute to the minute in the list
         if now.hour == int(pill1_hour_list[x]) and now.minute == int(pill1_minute_list[x]):
-            Motor1.ChangeDutyCycle(5)
+            Servo1.ChangeDutyCycle(5)
             sleep += pill1_numpills[x] * 1.3
             time.sleep(pill1_numpills[x] * 1.3)
-            Motor1.ChangeDutyCycle(0)
+            Servo1.ChangeDutyCycle(0)
             # Allows you to see it actually working
             print("Dispensing Pill")
             watering = True
+
     for x in range(len(pill2_hour_list)):
         # Compare the current hour to the hour in the list and the current minute to the minute in the list
         if now.hour == int(pill2_hour_list[x]) and now.minute == int(pill2_minute_list[x]):
             sleep += pill2_numpills[x] * 1.3
-            Motor2.ChangeDutyCycle(5)
+            Servo2.ChangeDutyCycle(5)
             time.sleep(pill2_numpills[x] * 1.3)
-            Motor2.ChangeDutyCycle(0)
+            Servo2.ChangeDutyCycle(0)
             # Allows you to see it actually working
             print("Dispensing Pill")
             watering = True
+
     for x in range(len(pill3_hour_list)):
         # Compare the current hour to the hour in the list and the current minute to the minute in the list
         if now.hour == int(pill3_hour_list[x]) and now.minute == int(pill3_minute_list[x]):
-            Motor3.ChangeDutyCycle(5)
+            Servo3.ChangeDutyCycle(5)
             sleep += pill3_numpills[x] * 1.3
             time.sleep(pill3_numpills[x] * 1.3)
-            Motor3.ChangeDutyCycle(0)
+            Servo3.ChangeDutyCycle(0)
             # Allows you to see it actually working
             print("Dispensing Pill")
             watering = True
+
+    for x in range(len(pill4_hour_list)):
+        # Compare the current hour to the hour in the list and the current minute to the minute in the list
+        if now.hour == int(pill4_hour_list[x]) and now.minute == int(pill4_minute_list[x]):
+            Servo4.ChangeDutyCycle(5)
+            sleep += pill4_numpills[x] * 1.3
+            time.sleep(pill4_numpills[x] * 1.3)
+            Servo4.ChangeDutyCycle(0)
+            # Allows you to see it actually working
+            print("Dispensing Pill")
+            watering = True
+
+    for x in range(len(pill5_hour_list)):
+        # Compare the current hour to the hour in the list and the current minute to the minute in the list
+        if now.hour == int(pill5_hour_list[x]) and now.minute == int(pill5_minute_list[x]):
+            sleep += pill5_numpills[x] * 1.3
+            Servo5.ChangeDutyCycle(5)
+            time.sleep(pill5_numpills[x] * 1.3)
+            Servo5.ChangeDutyCycle(0)
+            # Allows you to see it actually working
+            print("Dispensing Pill")
+            watering = True
+
+    for x in range(len(pill6_hour_list)):
+        # Compare the current hour to the hour in the list and the current minute to the minute in the list
+        if now.hour == int(pill6_hour_list[x]) and now.minute == int(pill6_minute_list[x]):
+            Servo6.ChangeDutyCycle(5)
+            sleep += pill6_numpills[x] * 1.3
+            time.sleep(pill6_numpills[x] * 1.3)
+            Servo6.ChangeDutyCycle(0)
+            # Allows you to see it actually working
+            print("Dispensing Pill")
+            watering = True
+
     if watering:
         arduinoSerialData.write('y')
         sleep += 8
-        water.ChangeDutyCycle(5)
+        GPIO.output(waterMotor, GPIO.HIGH)
         time.sleep(6)
-        water.ChangeDutyCycle(0)
+        GPIO.output(waterMotor, GPIO.LOW)
         os.system('pills.mp3')
     else:
         arduinoSerialData.write('n')
